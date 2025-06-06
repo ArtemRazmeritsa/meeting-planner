0c6e9e524d54e61504efffd2b7f2baffb2e3f13b
ArtemRazmeritsa <artemrazmeritsa@yandex.ru>
2025-06-06 15:36:52 +0300

    test(protect-router test): implement test for protected router

---
+++ src/app/routes/Protected-route.test.tsx
@@ -0,0 +1,40 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import ProtectedRoute from './Protected-route';
+import { User } from 'firebase/auth';
+import { useAuthStore } from '@/shared/global-stores/auth/use-auth-store';
+import { render, screen } from '@testing-library/react';
+import { ROUTES } from '@/shared/model/routes';
+
+const mockUser = {
+  uid: '123',
+  email: 'test@test.com',
+} as unknown as User;
+
+vi.mock('react-router-dom', () => ({
+  Outlet: () => <div>Outlet</div>,
+  Navigate: ({ to }: { to: string }) => <div>Navigate to {to}</div>,
+  useLocation: vi.fn(() => ({
+    pathname: '/',
+    search: '',
+    hash: '',
+    state: null,
+    key: 'default',
+  })),
+}));
+
+describe('ProtectedRoute', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('рендерит Outlet если пользователь авторизован', () => {
+    useAuthStore.setState({ user: mockUser, isLoading: false });
+    render(<ProtectedRoute />);
+    expect(screen.getByText('Outlet')).toBeTruthy();
+  });
+  it('пенренаправляет на страницу логина если пользователь не авторизован', () => {
+    useAuthStore.setState({ user: null, isLoading: false });
+    render(<ProtectedRoute />);
+    expect(screen.getByText(`Navigate to ${ROUTES.LOGIN}`)).toBeTruthy();
+  });
+});
