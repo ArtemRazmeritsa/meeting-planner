886063437a44ed92343d56dd3e3a09776ab021da
ArtemRazmeritsa <artemrazmeritsa@yandex.ru>
2025-06-27 23:23:30 +0300

    feat: create toggle theme hook and integrate it into AppHeader

--- src/features/AppHeader/index.tsx
+++ src/features/AppHeader/index.tsx
@@ -2,17 +2,22 @@ import { useAuthStore } from '@/shared/global-stores/auth/use-auth-store';
 import { ROUTES } from '@/shared/config/routes';
 import { Button } from '@/shared/ui/kit/button';
 import { useNavigate } from 'react-router-dom';
+import { Switch } from '@/shared/ui/kit/switch';
+import useToggleTheme from './model/use-toggle-theme';
+
+
 
 export function AppHeader() {
   const { user, logout } = useAuthStore();
   const navigate = useNavigate();
+  const {toggleTheme} = useToggleTheme()
 
   return (
     <header className="bg-secondary shadow-sm shadow-secondary py-4 px-4 mb-6">
       <div className="max-w-6xl mx-auto flex items-center justify-between">
         <Button
           variant="ghost"
-          className="text-xl text-primary-foreground/80 font-semibold "
+          className="text-xl text-foreground font-semibold "
           onClick={() => navigate(ROUTES.HOME)}
         >
           Meeting Planner
@@ -26,6 +31,7 @@ export function AppHeader() {
               </span>
             )}
           </span>
+          <Switch onClick={toggleTheme}></Switch>
           <Button
             variant="default"
             className="text-primary-foreground/80 shadow-secondary-foreground/80 hover:bg-accent-foreground"
---
+++ src/features/AppHeader/model/use-toggle-theme.ts
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+
+function useToggleTheme() {
+  useEffect(() => {
+    if (localStorage.getItem('theme') === 'dark') {
+      document.body.classList.add('dark');
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const dark = document.body.classList.toggle('dark')
+    localStorage.setItem('theme', dark ? 'dark' : 'default')
+  }
+
+  return {toggleTheme}
+}
+export default useToggleTheme;
